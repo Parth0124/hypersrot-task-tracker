a0da4f623b99d8edef9951ba4616e472fdeb10b2
Abhiraj412 <abhangparth@gmail.com>
2024-03-31 20:52:20 +0530

    commit

--- src/Pages/Todos.jsx
+++ src/Pages/Todos.jsx
@@ -25,7 +25,7 @@ const Todos = () => {
   };
 
   return (
-    <div className='container border border-4 rounded-3 shadow p-4 mb-5'>
+    <div className="container border border-4 rounded-3 shadow p-4 mb-5">
       <div className="row mb-4">
         <div className="col-md-12">
           {/* Filter Options */}
@@ -33,19 +33,30 @@ const Todos = () => {
             <div className="filterOptions d-flex justify-content-between mb-3">
               <div className="d-lg-flex flex-lg-row d-md-flex flex-md-row gap-4 d-sm-flex flex-sm-col">
                 <h6>Filter By: </h6>
-             
-                  <input type="text" name="assignee" id="assignee" placeholder='Assignee Name' className='border border-0 rounded-2 px-lg-3 mx-sm-3' value={searchQuery} onChange={handleSearchChange}/>
-                  {/* <select name='priority' id='priority' className='outline-0 border border-0 rounded-2 px-lg-3 mx-sm-3 text-secondary' value={priorityFilter} onChange={handlePriorityFilterChange}>
+
+                <input
+                  type="text"
+                  name="assignee"
+                  id="assignee"
+                  placeholder="Assignee Name"
+                  className="border border-0 rounded-2 px-lg-3 mx-sm-3"
+                  value={searchQuery}
+                  onChange={handleSearchChange}
+                />
+                {/* <select name='priority' id='priority' className='outline-0 border border-0 rounded-2 px-lg-3 mx-sm-3 text-secondary' value={priorityFilter} onChange={handlePriorityFilterChange}>
                     <option selected value="all">Priority</option>                    
                     <option value="P0">P0</option>
                     <option value="P1">P1</option>
                     <option value="P2">P2</option>
                   </select> */}
-                 
               </div>
               {/* Add New Task Button */}
               <div className="d-none d-sm-none d-block d-lg-block">
-                <button type='button' className='btnColor rounded-1 border border-0 text-white fw-semibold px-5' onClick={handleAddTask}>
+                <button
+                  type="button"
+                  className="btnColor rounded-1 border border-0 text-white fw-semibold px-5"
+                  onClick={handleAddTask}
+                >
                   Add New Task
                 </button>
               </div>
@@ -53,8 +64,16 @@ const Todos = () => {
             {/* Sort By Options */}
             <div className="d-flex gap-4 align-items-center">
               <h6>Sort By: </h6>
-              <select name='priority' id='priority' className='outline-0 border border-0 rounded-2 px-3 text-secondary' value={priorityFilter} onChange={handlePriorityFilterChange}>
-                <option selected value="all">Select</option>                    
+              <select
+                name="priority"
+                id="priority"
+                className="outline-0 border border-0 rounded-2 px-3 text-secondary"
+                value={priorityFilter}
+                onChange={handlePriorityFilterChange}
+              >
+                <option selected value="all">
+                  Select
+                </option>
                 <option value="P0">P0</option>
                 <option value="P1">P1</option>
                 <option value="P2">P2</option>
@@ -67,21 +86,31 @@ const Todos = () => {
       {/* DisplayTodos Component */}
       <div className="row">
         <div className="col-md-12">
-          <DisplayTodos searchQuery={searchQuery} priorityFilter={priorityFilter} dateRangeFilter={dateRangeFilter}/>
+          <DisplayTodos
+            searchQuery={searchQuery}
+            priorityFilter={priorityFilter}
+            dateRangeFilter={dateRangeFilter}
+          />
         </div>
       </div>
 
       {/* AddTask Component */}
       <div className="row">
-        <div className="d-block d-sm-block d-xs-block d-md-block d-none d-lg-none w-50 m-auto mt-5"> {/* This ensures the button takes full width on small screens */}
-          <button type='button' className='btnColor rounded-1 border border-0 text-white fw-semibold px-5 py-sm-1 w-100' onClick={handleAddTask}>
+        <div className="d-block d-sm-block d-xs-block d-md-block d-none d-lg-none w-50 m-auto mt-5">
+          {" "}
+          {/* This ensures the button takes full width on small screens */}
+          <button
+            type="button"
+            className="btnColor rounded-1 border border-0 text-white fw-semibold px-5 btn btn-dark"
+            onClick={handleAddTask}
+          >
             Add New Task
           </button>
         </div>
       </div>
 
       {/* Modal backdrop */}
-      <div className={`${showModal ? 'modal-backdrop fade show' : ''}`}></div>
+      <div className={`${showModal ? "modal-backdrop fade show" : ""}`}></div>
       <AddTask showModal={showModal} handleClose={handleCloseModal} />
     </div>
   );
