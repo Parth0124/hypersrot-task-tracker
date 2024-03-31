0c7805b250bd857cb35003211e73589f9bd64e9f
Abhiraj412 <abhangparth@gmail.com>
2024-03-31 18:06:19 +0530

    commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Header from './Header'
 import Todos from './Todos'
-import Footer from './Footer'
 
 function Home() {
   return (
     <div>
       <Header/>
       <Todos/>
-      <Footer />
     </div>
   )
 }
